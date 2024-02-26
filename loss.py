3768e46d73675f77e6f03f3bb46f8e98ac74d0cf
Vlad382 <vladtrifono@gmail.com>
2024-02-26 15:08:23 +0000

    Loss LLT update

--- loss.py
+++ loss.py
@@ -1,4 +1,7 @@
 import jax.numpy as jnp
 
 def LLT_loss(L, x, b):
-    return jnp.linalg.norm(L @ (L.T @ x) - b, ord=2)
+    return jnp.square(jnp.linalg.norm(L @ (L.T @ x) - b, ord=2))
+
+def Notay_loss():
+    pass
