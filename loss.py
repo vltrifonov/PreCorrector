8f56e076a2483566f866c56508c8fe4f4ba8dac7
Vlad382 <vladtrifono@gmail.com>
2024-03-19 13:38:43 +0000

    Notay loss added

--- loss.py
+++ loss.py
@@ -6,11 +6,17 @@ def LLT_loss(L, x, b):
     "L should be dense (and not batched since function is vmaped)"
     return jnp.square(jnp.linalg.norm(L @ (L.T @ x) - b, ord=2))
 
-def Notay_loss(**kwargs):
-    pass
+@jsparse.sparsify
+def Notay_loss(Linv, A, Ainv, res):
+    Ainv_res = Ainv @ res
+    num = Linv @ (Linv.T @ res) - Ainv_res
+    num = jnp.dot(num, jnp.dot(A, num))
+    denom = jnp.dot(Ainv_res, jnp.dot(A, Ainv_res))
+    return jnp.sqrt(num / denom)
 
 def mse_loss(L, A):
-    return jnp.square(jnp.linalg.norm(L @ L.T - A.todense(), ord=2))
+    return jnp.square(jnp.linalg.norm(L @ L.T - A.todense(), ord='fro'))
+
 
 # def LLT_loss(L, x, b):
 #     #TODO
--- train.py
+++ train.py
@@ -1,10 +1,34 @@
-from jax import vmap, lax
+from jax import vmap, lax, jit, random
 from jax.experimental import sparse as jsparse
 import jax.numpy as jnp
 import equinox as eqx
 
-from loss import LLT_loss, mse_loss
+from loss import LLT_loss, mse_loss, Notay_loss
 
+
+vinv = vmap(jnp.linalg.inv, in_axes=(0), out_axes=(0))
+
+def compute_loss_Notay(model, X, y):
+    '''Graph was made out of lhs A.
+       Positions in `X`:
+         X[0] - nodes of the graph.
+         X[1] - edges of the graph.
+         X[2] - receivers of the graph.
+         X[3] - senders of the graph.
+         X[4] - indices of bi-directional edges in the graph.
+         X[5] - solution of linear system x.
+         X[6] - rhs b.
+         X[7] - lhs A.
+         X[8] - key for random.normal residuals r.
+    '''  
+    L = vmap(model, in_axes=(0, 0, 0, 0, 0), out_axes=(0))(X[0], X[1], X[2], X[3], X[4])
+    Linv = vinv(L.todense())
+    A = X[7].todense()
+    Ainv = vinv(A)    
+#     res = vmap(random.normal, in_axes=(0, None), out_axes=(0))(X[8], X[6].shape[-1:])
+    loss = vmap(Notay_loss, in_axes=(0, 0, 0, 0), out_axes=(0))(Linv, A, Ainv, X[6])
+    return jnp.mean(loss)
+    
 def compute_loss_LLT(model, X, y):
     '''Graph was made out of lhs A.
        Positions in `X`:
@@ -25,6 +49,17 @@ def compute_loss_LLT_with_cond(model, X, y):
     loss = vmap(LLT_loss, in_axes=(0, 0, 0), out_axes=(0))(L, X[5], X[6])
 #     def cond_LLT(L): return jnp.linalg.cond(L @ L.T)
 #     cond_LLT = jsparse.sparsify(vmap(lambda L: jnp.linalg.cond(L @ L.T), in_axes=(0), out_axes=(0)))(L)
+    cond_LLT = vmap(lambda L: jnp.linalg.cond(L @ L.T), in_axes=(0), out_axes=(0))(L.todense())
+    return jnp.sum(loss), jnp.mean(cond_LLT)
+
+def compute_loss_Notay_with_cond(model, X, y):
+    L = vmap(model, in_axes=(0, 0, 0, 0, 0), out_axes=(0))(X[0], X[1], X[2], X[3], X[4])
+    Linv = vinv(L.todense())
+    A = X[7].todense()
+    Ainv = vinv(A)    
+#     res = vmap(random.normal, in_axes=(0, None), out_axes=(0))(X[8], X[6].shape[-1:])
+    loss = vmap(Notay_loss, in_axes=(0, 0, 0, 0), out_axes=(0))(Linv, A, Ainv, X[6])
+    
     cond_LLT = vmap(lambda L: jnp.linalg.cond(L @ L.T), in_axes=(0), out_axes=(0))(L.todense())
     return jnp.sum(loss), jnp.mean(cond_LLT)
 
@@ -65,17 +100,20 @@ def train(model, data, train_config, compute_loss):
         return loss, model, opt_state
     
     def make_val_step(model, X, y):
-#         loss = compute_loss(model, X, y)
+        loss = compute_loss(model, X, y)
         loss, cond = compute_loss_LLT_with_cond(model, X, y)
         return loss, cond
+#         return loss
     
     def train_body(carry, x):
         model, opt_state = carry
         loss_train, model, opt_state = make_step(model, X_train, y_train, opt_state)
+#         loss_test = make_val_step(model, X_test, y_test)
         loss_test, cond_test = make_val_step(model, X_test, y_test)
         carry = (model, opt_state)
+#         return carry, [loss_train, loss_test]
         return carry, [loss_train, loss_test, cond_test]
-    
+   
     carry_init = (model, opt_state)
     (model, _), losses = lax.scan(train_body, carry_init, None, length=train_config['epoch_num'])
     return model, losses
