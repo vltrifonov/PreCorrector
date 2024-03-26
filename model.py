f2f66de0f0f34f6f037e745d00b093f85a669bfa
Vlad382 <vladtrifono@gmail.com>
2024-03-26 16:14:43 +0000

    Each diagonal handling in predicted L has its own class

--- model.py
+++ model.py
@@ -4,39 +4,11 @@ from jax import random, vmap
 import jax.numpy as jnp
 import jax.tree_util as tree
 import jax.nn as jnn
+from jax.experimental import sparse as jsparse
 
 from jax.ops import segment_sum
 import equinox as eqx
 
-from utils import graph_to_low_tri_mat_sparse, graph_tril, graph_to_low_tri_mat
-from data import bi_direc_edge_avg
-
-class PrecNet(eqx.Module):
-    NodeEncoder: eqx.Module
-    EdgeEncoder: eqx.Module
-    MessagePass: eqx.Module
-    EdgeDecoder: eqx.Module
-
-    def __init__(self, NodeEncoder, EdgeEncoder, MessagePass, EdgeDecoder):
-        super(PrecNet, self).__init__()
-        self.NodeEncoder = NodeEncoder
-        self.EdgeEncoder = EdgeEncoder
-        self.MessagePass = MessagePass
-        self.EdgeDecoder = EdgeDecoder
-        return
-    
-    def __call__(self, nodes, edges, receivers, senders, bi_edges_indx):
-        nodes = self.NodeEncoder(nodes[None, ...])
-        edges = self.EdgeEncoder(edges[None, ...])
-        nodes, edges, receivers, senders = self.MessagePass(nodes, edges, receivers, senders)
-        edges = bi_direc_edge_avg(edges, bi_edges_indx)
-        edges = self.EdgeDecoder(edges)
-        nodes, edges, receivers, senders = graph_tril(nodes, jnp.squeeze(edges), receivers, senders)
-        low_tri = graph_to_low_tri_mat_sparse(nodes, edges, receivers, senders)
-#         low_tri = graph_to_low_tri_mat(nodes, jnp.squeeze(edges), receivers, senders)
-        return low_tri
-    
-    
 class FullyConnectedNet(eqx.Module):
     layers: list
     act: Callable = eqx.field(static=True)
@@ -57,7 +29,8 @@ class FullyConnectedNet(eqx.Module):
         x = self.layers[-1](x)
         return x
     
-class MessagePassing(eqx.Module):
+    
+class MessagePassingNoDiag(eqx.Module):
     update_edge_fn: eqx.Module
     update_node_fn: eqx.Module
     aggregate_edges_for_nodes_fn: Callable = eqx.field(static=True)
@@ -96,7 +69,7 @@ class MessagePassing(eqx.Module):
         sent_attributes = tree.tree_map(lambda n: n[:, senders], nodes)
         received_attributes = tree.tree_map(lambda n: n[:, receivers], nodes)
 
-        # Find non-main-diagonal edges
+#         # Find non-main-diagonal edges
         non_diag_edge_idx = jnp.diff(jnp.hstack([senders[:, None], receivers[:, None]]))
         non_diag_edge_idx = jnp.nonzero(non_diag_edge_idx, size=senders.shape[0]-nodes.shape[1], fill_value=jnp.nan)[0].astype(jnp.int32)
         
@@ -107,5 +80,46 @@ class MessagePassing(eqx.Module):
         ], axis=0)
         edges_upd = self.update_edge_fn(feat_in)
         edges = edges.at[:, non_diag_edge_idx].set(edges_upd)
-#         edges = self.update_edge_fn(jnp.concatenate([edges, sent_attributes, received_attributes], axis=0))
+        return edges
+
+    
+class MessagePassingWithDiag(eqx.Module):
+    update_edge_fn: eqx.Module
+    update_node_fn: eqx.Module
+    aggregate_edges_for_nodes_fn: Callable = eqx.field(static=True)
+    mp_rounds: int = eqx.field(static=True)
+        
+    def __init__(self, update_edge_fn, update_node_fn, mp_rounds, aggregate_edges_for_nodes_fn=segment_sum):
+        super(MessagePassing, self).__init__()
+        self.update_edge_fn = update_edge_fn
+        self.update_node_fn = update_node_fn
+        self.aggregate_edges_for_nodes_fn = aggregate_edges_for_nodes_fn
+        self.mp_rounds = mp_rounds
+        return        
+        
+    def __call__(self, nodes, edges, receivers, senders):
+        for _ in range(self.mp_rounds):
+            nodes = self._update_nodes(nodes, edges, receivers, senders)
+            edges = self._update_edges(nodes, edges, receivers, senders)
+        return nodes, edges, receivers, senders
+    
+    def _update_nodes(self, nodes, edges, receivers, senders):
+        sum_n_node = tree.tree_leaves(nodes)[0].shape[1]
+        sent_attributes = vmap(
+            tree.tree_map, 
+            in_axes=(None, 0), out_axes=(0)
+        )(lambda e: self.aggregate_edges_for_nodes_fn(e, senders, sum_n_node), edges)
+    
+        received_attributes = vmap(
+            tree.tree_map, 
+            in_axes=(None, 0), out_axes=(0)
+        )(lambda e: self.aggregate_edges_for_nodes_fn(e, receivers, sum_n_node), edges)
+        
+        nodes = self.update_node_fn(jnp.concatenate([nodes, sent_attributes, received_attributes], axis=0))
+        return nodes
+    
+    def _update_edges(self, nodes, edges, receivers, senders):
+        sent_attributes = tree.tree_map(lambda n: n[:, senders], nodes)
+        received_attributes = tree.tree_map(lambda n: n[:, receivers], nodes)
+        edges = self.update_edge_fn(jnp.concatenate([edges, sent_attributes, received_attributes], axis=0))
         return edges
---
+++ prec_models.py
@@ -0,0 +1,169 @@
+import jax.numpy as jnp
+from jax.experimental import sparse as jsparse
+import equinox as eqx
+
+from utils import graph_to_low_tri_mat_sparse, graph_tril, graph_to_low_tri_mat
+from data import bi_direc_edge_avg
+
+class PrecNetLearnAll(eqx.Module):
+    NodeEncoder: eqx.Module
+    EdgeEncoder: eqx.Module
+    MessagePass: eqx.Module
+    EdgeDecoder: eqx.Module
+
+    def __init__(self, NodeEncoder, EdgeEncoder, MessagePass, EdgeDecoder):
+        super(PrecNet, self).__init__()
+        self.NodeEncoder = NodeEncoder
+        self.EdgeEncoder = EdgeEncoder
+        self.MessagePass = MessagePass
+        self.EdgeDecoder = EdgeDecoder
+        return
+    
+    def __call__(self, nodes, edges, receivers, senders, bi_edges_indx):
+        nodes = self.NodeEncoder(nodes[None, ...])
+        edges = self.EdgeEncoder(edges[None, ...])
+        nodes, edges, receivers, senders = self.MessagePass(nodes, edges, receivers, senders)
+        edges = bi_direc_edge_avg(edges, bi_edges_indx)
+        edges = self.EdgeDecoder(edges)
+        nodes, edges, receivers, senders = graph_tril(nodes, jnp.squeeze(edges), receivers, senders)
+        low_tri = graph_to_low_tri_mat_sparse(nodes, edges, receivers, senders)
+        return low_tri
+
+    
+class PrecNetCopyDiag(eqx.Module):
+    NodeEncoder: eqx.Module
+    EdgeEncoder: eqx.Module
+    MessagePass: eqx.Module
+    EdgeDecoder: eqx.Module
+
+    def __init__(self, NodeEncoder, EdgeEncoder, MessagePass, EdgeDecoder):
+        super(PrecNet, self).__init__()
+        self.NodeEncoder = NodeEncoder
+        self.EdgeEncoder = EdgeEncoder
+        self.MessagePass = MessagePass
+        self.EdgeDecoder = EdgeDecoder
+        return
+    
+    def __call__(self, nodes, edges, receivers, senders, bi_edges_indx):
+         # Save main diagonal
+        diag_edge_indx = jnp.diff(jnp.hstack([senders[:, None], receivers[:, None]]))
+        diag_edge_indx = jnp.where(diag_edge_indx == 0, 1, 0)
+        diag_edge_indx = jnp.nonzero(diag_edge_indx, size=nodes.shape[0], fill_value=jnp.nan)[0].astype(jnp.int32)
+        diag_edge = edges.at[diag_edge_indx].get()
+        
+        nodes = self.NodeEncoder(nodes[None, ...])
+        edges = self.EdgeEncoder(edges[None, ...])
+        nodes, edges, receivers, senders = self.MessagePass(nodes, edges, receivers, senders)
+        edges = bi_direc_edge_avg(edges, bi_edges_indx)
+        edges = self.EdgeDecoder(edges)
+        edges = edges.at[:, diag_edge_indx].set(diag_edge)
+        
+        nodes, edges, receivers, senders = graph_tril(nodes, jnp.squeeze(edges), receivers, senders)
+        low_tri = graph_to_low_tri_mat_sparse(nodes, edges, receivers, senders)
+        return low_tri
+    
+    
+class PrecNetCopyDiagSqrt(eqx.Module):
+    NodeEncoder: eqx.Module
+    EdgeEncoder: eqx.Module
+    MessagePass: eqx.Module
+    EdgeDecoder: eqx.Module
+
+    def __init__(self, NodeEncoder, EdgeEncoder, MessagePass, EdgeDecoder):
+        super(PrecNet, self).__init__()
+        self.NodeEncoder = NodeEncoder
+        self.EdgeEncoder = EdgeEncoder
+        self.MessagePass = MessagePass
+        self.EdgeDecoder = EdgeDecoder
+        return
+    
+    def __call__(self, nodes, edges, receivers, senders, bi_edges_indx):
+         # Save main diagonal
+        diag_edge_indx = jnp.diff(jnp.hstack([senders[:, None], receivers[:, None]]))
+        diag_edge_indx = jnp.where(diag_edge_indx == 0, 1, 0)
+        diag_edge_indx = jnp.nonzero(diag_edge_indx, size=nodes.shape[0], fill_value=jnp.nan)[0].astype(jnp.int32)
+        diag_edge = edges.at[diag_edge_indx].get()
+        
+        nodes = self.NodeEncoder(nodes[None, ...])
+        edges = self.EdgeEncoder(edges[None, ...])
+        nodes, edges, receivers, senders = self.MessagePass(nodes, edges, receivers, senders)
+        edges = bi_direc_edge_avg(edges, bi_edges_indx)
+        edges = self.EdgeDecoder(edges)
+        edges = edges.at[:, diag_edge_indx].set(jnp.sqrt(diag_edge))
+        
+        nodes, edges, receivers, senders = graph_tril(nodes, jnp.squeeze(edges), receivers, senders)
+        low_tri = graph_to_low_tri_mat_sparse(nodes, edges, receivers, senders)
+        return low_tri
+
+    
+class PrecNetRigidLDLT(eqx.Module):
+    NodeEncoder: eqx.Module
+    EdgeEncoder: eqx.Module
+    MessagePass: eqx.Module
+    EdgeDecoder: eqx.Module
+
+    def __init__(self, NodeEncoder, EdgeEncoder, MessagePass, EdgeDecoder):
+        super(PrecNet, self).__init__()
+        self.NodeEncoder = NodeEncoder
+        self.EdgeEncoder = EdgeEncoder
+        self.MessagePass = MessagePass
+        self.EdgeDecoder = EdgeDecoder
+        return
+    
+    def __call__(self, nodes, edges, receivers, senders, bi_edges_indx):
+         # Save main diagonal
+        diag_edge_indx = jnp.diff(jnp.hstack([senders[:, None], receivers[:, None]]))
+        diag_edge_indx = jnp.where(diag_edge_indx == 0, 1, 0)
+        diag_edge_indx = jnp.nonzero(diag_edge_indx, size=nodes.shape[0], fill_value=jnp.nan)[0].astype(jnp.int32)
+        diag_edge = edges.at[diag_edge_indx].get()
+        D = jsparse.eye(diag_edge.shape[0]) * diag_edge
+        
+        nodes = self.NodeEncoder(nodes[None, ...])
+        edges = self.EdgeEncoder(edges[None, ...])
+        nodes, edges, receivers, senders = self.MessagePass(nodes, edges, receivers, senders)
+        edges = bi_direc_edge_avg(edges, bi_edges_indx)
+        edges = self.EdgeDecoder(edges)
+        
+        edges = edges.at[:, diag_edge_indx].set(1)
+        
+        nodes, edges, receivers, senders = graph_tril(nodes, jnp.squeeze(edges), receivers, senders)
+        low_tri = graph_to_low_tri_mat_sparse(nodes, edges, receivers, senders)
+        return low_tri, D
+
+
+class PrecNetLDLT(eqx.Module):
+    #TODO
+    NodeEncoder: eqx.Module
+    EdgeEncoder: eqx.Module
+    MessagePass: eqx.Module
+    EdgeDecoder: eqx.Module
+
+    def __init__(self, NodeEncoder, EdgeEncoder, MessagePass, EdgeDecoder):
+        super(PrecNet, self).__init__()
+        self.NodeEncoder = NodeEncoder
+        self.EdgeEncoder = EdgeEncoder
+        self.MessagePass = MessagePass
+        self.EdgeDecoder = EdgeDecoder
+        return
+    
+    def __call__(self, nodes, edges, receivers, senders, bi_edges_indx):
+         # Save main diagonal
+        diag_edge_indx = jnp.diff(jnp.hstack([senders[:, None], receivers[:, None]]))
+        diag_edge_indx = jnp.where(diag_edge_indx == 0, 1, 0)
+        diag_edge_indx = jnp.nonzero(diag_edge_indx, size=nodes.shape[0], fill_value=jnp.nan)[0].astype(jnp.int32)
+        diag_edge = edges.at[diag_edge_indx].get()
+        D = jsparse.eye(diag_edge.shape[0]) * diag_edge
+        
+        nodes = self.NodeEncoder(nodes[None, ...])
+        edges = self.EdgeEncoder(edges[None, ...])
+        nodes, edges, receivers, senders = self.MessagePass(nodes, edges, receivers, senders)
+        edges = bi_direc_edge_avg(edges, bi_edges_indx)
+        edges = self.EdgeDecoder(edges)
+        nodes, edges, receivers, senders = graph_tril(nodes, jnp.squeeze(edges), receivers, senders)
+        low_tri = graph_to_low_tri_mat_sparse(nodes, edges, receivers, senders)
+  
+         # Normalize L
+#         diag_edge_to_norm = edges.at[diag_edge_indx].get()
+#         S_inv = jsparse.eye(diag_edge_to_norm.shape[0]) * (1 / diag_edge_to_norm)
+#         low_tri = jsparse.sparsify(lambda A, B: A @ B)(low_tri, S_inv)
+        return low_tri, D, S_inv
