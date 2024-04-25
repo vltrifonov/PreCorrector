e5399dfcea0fcfd03266841d32ac4b59c9f7370e
Vlad382 <vladtrifono@gmail.com>
2024-04-25 16:42:38 +0000

    Sparse triangular solver compitable with jit transformation. Finally!

--- linsolve/spsolve_triangular.py
+++ linsolve/spsolve_triangular.py
@@ -1,54 +1,44 @@
 from functools import partial
-import jax
+from jax.lax import scan
 import jax.numpy as jnp
 
-@partial(jax.jit, static_argnums=(2,))
-def jspsolve_triangular(A: jax.experimental.sparse.bcoo.BCOO, b: jax.Array, lower: bool=True) -> jax.Array:
+@partial(jit, static_argnums=(2))
+def jspsolve_triangular(A, b, lower):
     '''A must be a lower/upper triangular matrix.
        It should be "valid": not singular (have no zeros on diagonal, no empty rows, etc.)'''
-    A_local, b_local = A, b
-    x = jnp.zeros_like(b_local)
-    dim_range = jnp.arange(x.shape[0]) if lower else jnp.arange(x.shape[0] - 1, -1, -1)
+    Aval, bval = A.data, b
+    Arows, Acols = A.indices[:, 0], A.indices[:, 1]
+    x = jnp.zeros_like(bval)    
     
-    for i in dim_range:
-        a_ij, a_ii = A_local[i, :].todense(), A_local[i, i].todense()
-        c = b_local[i] - jnp.sum(a_ij * x)
-        x = x.at[i].add(c)
-        x = x.at[i].divide(a_ii)
-    return x
-
-
-
-
-
-
-# TODO: materialize full row/columns with zeros on corresponding positions to elemnt-wise multiply by rhs (cannot get not conrecte shape when retrieving elements)
-# @partial(jax.jit, static_argnums=(2,))
-# def jspsolve_triangular_efficient(A: jax.experimental.sparse.bcoo.BCOO, b: jax.Array, lower: bool=True) -> jax.Array:
-#     '''A must be a lower/upper triangular matrix.
-#        It should be "valid": not singular (have no zeros on diagonal, no empty rows, etc.)'''
-#     Aval, bval = A.data, b
-#     Arows, Acols = A.indices[:, 0], A.indices[:, 1]
-#     x = jnp.copy(bval)
-
-#     if lower:
-#         f_pointer = partial(pointer_over_dim, iterate_dim=Arows, retrive_dim=Acols)
-#         dim_range = jnp.arange(1, x.shape[0])
-#         x = x.at[0].divide(Aval[0, 0])
-#     else:
-#         f_pointer = partial(pointer_over_dim, iterate_dim=Acols, retrive_dim=Arows)
-#         dim_range = jnp.arange(x.shape[0] - 2, -1, -1)
-#         x = x.at[-1].divide(Aval[-1])
+    diag_edge_indx = jnp.diff(jnp.hstack([Arows[:, None], Acols[:, None]]))
+    diag_edge_indx = jnp.where(diag_edge_indx == 0, 1, 0)
+    diag_edge_indx = jnp.nonzero(diag_edge_indx, size=nodes.shape[1], fill_value=jnp.nan)[0].astype(jnp.int32)
     
-#     for i in dim_range:
-#         val_ind = f_pointer(i)
-#         nondiag_ind, diag_ind = val_ind[:-1], val_ind[-1]
-#         c = jnp.sum(Aval.at[nondiag_ind].get() * x.at[nondiag_ind].get())
-#         x = x.at[i].add(-c)
+    if lower:
+        dim_range = jnp.arange(1, x.shape[0])
+        x = x.at[0].set(bval[0] / Aval[0])
+    else:
+        dim_range = jnp.arange(x.shape[0] - 2, -1, -1)
+        x = x.at[-1].divide(bval[-1] / Aval[-1])
+    
+#     for i, diag_ind in zip(dim_range, diag_edge_indx[1:]):
+#         nondiag_ind = jnp.where(Arows == i, 1, 0)
+#         x_i = x.at[jnp.where(nondiag_ind, Acols, Acols.shape[0])].get(mode='fill', fill_value=0)
+#         c = jnp.sum(Aval.at[nondiag_ind].get() * x_i)
+#         x = x.at[i].set(bval[i] - c)
 #         x = x.at[i].divide(Aval[diag_ind])
-#     return x
         
-# def pointer_over_dim(i, iterate_dim, retrive_dim):
-#     '''Returns all indices in the columns/row of the i-th row/column.'''
-# #     return retrive_dim[jnp.where(iterate_dim == i, 1, 0).astype(bool)]
-#     return jnp.nonzero(jnp.where(iterate_dim == i, 1, 0), size=i, fill_value=0)[0].astype(jnp.int32)
+    def f_(carry, k):
+        i, diag_ind = k
+        Aval_, Arows_, Acols_, bval_, x_ = carry
+        nondiag_ind = jnp.where(Arows_ == i, 1, 0)
+        x_i = x_.at[jnp.where(nondiag_ind, Acols_, Acols_.shape[0])].get(mode='fill', fill_value=0)
+        
+        c = jnp.sum(Aval_.at[nondiag_ind].get() * x_i)
+        x_ = x_.at[i].set(bval_[i] - c)
+        x_ = x_.at[i].divide(Aval_[diag_ind])
+        return (Aval_, Arows_, Acols_, bval_, x_), None
+    
+    carry_ = (Aval, Arows, Acols, bval, x)
+    (_, _, _, _, x), _ = scan(f_, carry_, jnp.hstack([dim_range[:, None], diag_edge_indx[1:][:, None]]))
+    return x
