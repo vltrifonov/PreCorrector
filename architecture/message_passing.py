6abcf0612f66965440197f4ee13dfaebd6ed1803
Vlad382 <vladtrifono@gmail.com>
2025-01-13 09:40:46 +0000

    Message passing with options for nodes initialization and diagonal pinning

--- architecture/message_passing.py
+++ architecture/message_passing.py
@@ -12,16 +12,20 @@ class MessagePassing_StaticDiag(eqx.Module):
     update_node_fn: eqx.Module
     aggregate_edges_for_nodes_fn: Callable = eqx.field(static=True)
     mp_rounds: int = eqx.field(static=True)
+    nodes_init_fn: Callable = eqx.field(static=True)
         
-    def __init__(self, update_edge_fn, update_node_fn, mp_rounds, aggregate_edges_for_nodes_fn=segment_sum):
-        super(MessagePassing, self).__init__()
+    def __init__(self, update_edge_fn, update_node_fn, mp_rounds,
+                 nodes_init_fn, aggregate_edges_for_nodes_fn=segment_sum):
+        super(MessagePassing_StaticDiag, self).__init__()
         self.update_edge_fn = update_edge_fn
         self.update_node_fn = update_node_fn
         self.aggregate_edges_for_nodes_fn = aggregate_edges_for_nodes_fn
         self.mp_rounds = mp_rounds
+        self.nodes_init_fn = nodes_init_fn
         return        
         
     def __call__(self, nodes, edges, receivers, senders):
+        nodes = self.nodes_init_fn(nodes)
         for _ in range(self.mp_rounds):
             nodes = self._update_nodes(nodes, edges, receivers, senders)
             edges = self._update_edges(nodes, edges, receivers, senders)
@@ -55,6 +59,25 @@ class MessagePassing_StaticDiag(eqx.Module):
             edges[:, non_diag_edge_idx],
             sent_attributes[:, non_diag_edge_idx],
             received_attributes[:, non_diag_edge_idx]
+        ], axis=0)
+        edges_upd = self.update_edge_fn(feat_in)        
+        edges = edges.at[:, non_diag_edge_idx].set(edges_upd, mode='drop')
+        return edges
+    
+class MessagePassing_NotStaticDiag(MessagePassing_StaticDiag):
+    def __init__(self, update_edge_fn, update_node_fn, mp_rounds,
+                 nodes_init_fn, aggregate_edges_for_nodes_fn=segment_sum):
+        super().__init__(update_edge_fn, update_node_fn, mp_rounds,
+                 nodes_init_fn, aggregate_edges_for_nodes_fn=segment_sum)
+    
+    def _update_edges(self, nodes, edges, receivers, senders):
+        sent_attributes = tree.tree_map(lambda n: n[:, senders], nodes)
+        received_attributes = tree.tree_map(lambda n: n[:, receivers], nodes)
+        
+        feat_in = jnp.concatenate([
+            edges,
+            sent_attributes,
+            received_attributes,
         ], axis=0)
         edges_upd = self.update_edge_fn(feat_in)        
         edges = edges.at[:, non_diag_edge_idx].set(edges_upd, mode='drop')
--- data/dataset.py
+++ data/dataset.py
@@ -1,6 +1,6 @@
 from data.qtt import load_pde_data
 
-def dataset_qtt(pde, grid, variance, lhs_type, return_train, N_samples, precision='f32', fill_factor=None, threshold=None, power=None):
+def dataset_qtt(pde, grid, variance, lhs_type, return_train, N_samples, precision='f64', fill_factor=None, threshold=None, power=None):
     A, A_pad, b, x, bi_edges = load_pde_data(pde, grid, variance, lhs_type, return_train, N_samples=N_samples,
                                              fill_factor=fill_factor, threshold=threshold, power=power, precision=precision)
     return A, A_pad, b, x, bi_edges
