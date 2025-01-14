8858ae49df1abc30e3862af822b15d2c719d5a17
Vlad382 <vladtrifono@gmail.com>
2025-01-14 08:09:22 +0000

    Scipy linear soler module cleaned up; pre-time is an argument of the cg function

--- scipy_linsolve.py
+++ scipy_linsolve.py
@@ -6,55 +6,40 @@ import numpy as np
 from time import perf_counter
 from functools import partial
 import inspect
-from utils import jBCOO_to_scipyCSR, iter_per_residual
+from utils import jBCOO_to_scipyCSR
 
-def batched_cg_scipy(A, b, P=None, atol=1e-12, maxiter=1000, x0='random'):
-    assert (x0 == 'random') | (x0 == None)
-    iters_ls = [[], [], [], []]
-    time_ls = [[], [], [], []]
+def batched_cg_scipy(A, b, pre_time, P=None, atol=1e-12, maxiter=1000, thresholds=[1e-3, 1e-6, 1e-9, 1e-12]):
+    # results_array \in R^{linsystem_num x threshold_num x 2}, where 2 is for residulas and time to tol
+    results_array = np.full([A.shape[0], len(thresholds), 2], -42)
     P = P if P else [None]*A.shape[0]
     
     for i in range(A.shape[0]):
         A_i, b_i, P_i, = A[i, ...], b[i, ...], P[i]
-        sol, res_i, time_i = cg_scipy(jBCOO_to_scipyCSR(A_i), b_i, P_i, atol=atol, maxiter=maxiter, x0=x0)        
-        iters = iter_per_residual(res_i)
+        _, res_i, time_i = cg_scipy(jBCOO_to_scipyCSR(A_i), b_i, P_i, atol=atol, maxiter=maxiter)        
         
-        iters_ls[0].append(iters[1e-3])
-        iters_ls[1].append(iters[1e-6])
-        iters_ls[2].append(iters[1e-9])
-        iters_ls[3].append(iters[1e-12])
-        
-        if np.isnan(iters_ls[0][-1]):
-            print(f'{i} - alert')
-            continue
-        
-        time_ls[0].append(time_i[iters_ls[0][-1]])
-        time_ls[1].append(time_i[iters_ls[1][-1]])
-        time_ls[2].append(time_i[iters_ls[2][-1]])
-        time_ls[3].append(time_i[iters_ls[3][-1]])
+        for j, t in enumerate(thresholds):
+            try:
+                iters_to_res = np.where(cg_res <= t)[0][0]
+                time_to_res = cg_time[iters_to_res]
+            except:
+                iters_to_res, time_to_res = np.nan, np.nan
+            
+            iter_time_per_res[i, j, 0] = iters_to_res + 1
+            iter_time_per_res[i, j, 1] = time_to_res + pre_time
     
-    if np.isnan(iters_ls[0]).any():
-        print(f'All nans to 1e-3? {np.isnan(iters_ls[0]).all()}')
+    iters_mean, iters_std = {}, {}
+    time_mean, time_std = {}, {}
+    nan_flag = {}
+    for j, t in enumerate(thresholds):
+        iters_mean[t] = np.nanmean(results_array[:, j, 0])
+        iters_std[t] = np.nanstd(results_array[:, j, 0])
+        time_mean[t] = np.nanmean(results_array[:, j, 1])
+        time_std[t] = np.nanstd(results_array[:, j, 1])
+        nan_flag[t] = np.sum(np.isnan(results_array[:, j, 0]))
     
-    iters_mean = [
-        np.mean(iters_ls[0]), np.mean(iters_ls[1]), np.mean(iters_ls[2]), np.mean(iters_ls[3])
-    ]
-    iters_std = [
-        np.std(iters_ls[0]), np.std(iters_ls[1]), np.std(iters_ls[2]), np.std(iters_ls[3])
-    ]
-    time_mean = [
-        np.mean(time_ls[0]), np.mean(time_ls[1]), np.mean(time_ls[2]), np.mean(time_ls[3])
-    ]
-    time_std = [
-        np.std(time_ls[0]), np.std(time_ls[1]), np.std(time_ls[2]), np.std(time_ls[3])
-    ]
-    return sol, iters_mean, iters_std, time_mean, time_std
+    return iters_mean, iters_std, time_mean, time_std, nan_flag
 
-def cg_scipy(A, b, P, atol, maxiter, x0='random'):
-    assert (x0 == 'random') | (x0 == None)
-    A_loc = A
-    b_loc = np.array(b, dtype=np.float64)
-    
+def cg_scipy(A, b, P, atol, maxiter):
     res_nonlocal = []
     time_per_iter = []
     def residuals_callback(xk):
@@ -62,10 +47,11 @@ def cg_scipy(A, b, P, atol, maxiter, x0='random'):
         time_per_iter.append(perf_counter())
         peek = inspect.currentframe().f_back
         res_nonlocal.append(np.linalg.norm(peek.f_locals['r']))
-
-    x0 = np.array(random.normal(random.PRNGKey(42), b_loc.shape)) if x0 == 'random' else None
+    
+    x0 = None # Initialization with zero vector. Always x0 = None
     t_start = perf_counter()
-    solution, info = cg(A_loc, b_loc, M=P, callback=residuals_callback, rtol=0, atol=atol, maxiter=maxiter, x0=x0)
+    solution, info = cg(A, np.array(b, dtype=np.float64), M=P, callback=residuals_callback,
+                        rtol=0, atol=atol, maxiter=maxiter, x0=x0)
 
     res_nonlocal = np.array(res_nonlocal)
     time_per_iter = np.array(time_per_iter) - t_start
